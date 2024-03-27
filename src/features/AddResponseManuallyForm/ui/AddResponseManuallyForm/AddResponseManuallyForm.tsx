78f2f0338889180666238a2185c0105d3894d24c
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-03-27 10:00:43 +0800

    Добавлены хэндлеры и отправка отклика

--- src/features/AddResponseManuallyForm/ui/AddResponseManuallyForm/AddResponseManuallyForm.tsx
+++ src/features/AddResponseManuallyForm/ui/AddResponseManuallyForm/AddResponseManuallyForm.tsx
@@ -1,6 +1,9 @@
 import {Input} from "shared/ui/Input/Input";
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./AddResponseManuallyForm.module.css";
+import {useCallback, useState} from "react";
+import {addResponse} from "../../api/addResponse/addResponse";
+import {AddResponseSchema} from "../../model/types/AddResponseSchema";
 interface AddResponseManuallyFormProps {
     className?: string;
 }
@@ -8,13 +11,60 @@ export const AddResponseManuallyForm = (props: AddResponseManuallyFormProps) => 
     const {
         className
     } = props;
+
+    const [response, setResponse] = useState<AddResponseSchema>();
+    const [isLoading, setIsLoading] = useState(false);
+    const sendHandler = useCallback(async (params?: AddResponseSchema) => {
+        setIsLoading(true);
+        const res = await addResponse(
+            params || {}
+        );
+        if(res.result===1){
+            setResponse({});
+        }
+        setIsLoading(false);
+    }, []);
+
+    const changeFio = useCallback(
+        (value?: string) => {
+            setResponse((prevState) => ({...prevState, fio:value}));
+        },
+        [setResponse],
+    );
+    const changeJobTitle = useCallback(
+        (value?: string) => {
+            setResponse((prevState) => ({...prevState, jobTitle:value}));
+        },
+        [setResponse],
+    );
+    const changePhone = useCallback(
+        (value?: string) => {
+            setResponse((prevState) => ({...prevState, phone:value}));
+        },
+        [setResponse],
+    );
+    const changeEmail = useCallback(
+        (value?: string) => {
+            setResponse((prevState) => ({...prevState, email:value}));
+        },
+        [setResponse],
+    );
+
+    if(isLoading) {
+        return <div>Loading ...</div>;
+    }
     return (
         <div className={classNames(cls.form, {}, [className])}>
-            <Input label="ФИО" />
-            <Input label="Должность" />
-            <Input label="Телефон" />
-            <Input label="Email" />
-            <button>Записать отклик</button>
+            <Input className={cls.input} label="ФИО" onChange={changeFio}/>
+            <Input className={cls.input} label="Должность" onChange={changeJobTitle} />
+            <Input className={cls.input} label="Телефон" onChange={changePhone} />
+            <Input className={cls.input} label="Email" onChange={changeEmail} />
+
+            <button onClick={() => {
+                sendHandler(response);
+            }}>
+                Записать отклик
+            </button>
         </div>
     );
 };
